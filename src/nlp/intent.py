6464256475ab17f0eae0c0823ba3372bb2a88977
noczero <zeroonetm@gmail.com>
2023-05-06 12:01:25 +0700

    move package

--- src/nlp/intent.py
+++ src/nlp/intent.py
@@ -1,14 +1,10 @@
-from setfit import SetFitModel, SetFitTrainer
-
-from src.utils import LABEL_TO_INTENT_CLASS
+from src.nlp.utils import LABEL_TO_INTENT_CLASS, load_pickle_model
 
 
 class IntentModel:
 
     def __init__(self, model_name_or_path: str) -> None:
-        self.model = SetFitModel.from_pretrained(
-            model_name_or_path
-        )
+        self.model = load_pickle_model(model_name_or_path)
 
     def predict(self, prompt: str):
         label_prediction = self.model([prompt])
